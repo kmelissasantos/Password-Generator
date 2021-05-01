480ae5ede432964bd25013d828ef8506acbb59bc
Melissa Santos <melissasantos@Melissas-MBP.lan>
2021-05-01 12:52:47 -0400

    added password variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,44 @@
 // Assignment code here
 
+var enter;
+
+var confirmNumber;
+
+var confirmCharacter;
+
+var confirmUppercase;
+
+var confirmLowercase;
+
+// Start Password variable values:*
+
+// Special characters*
+
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+// Numeric characters*
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Alphabetical characters*
+
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Space is for the Uppercase conversion*
+
+space = [];
+
+// Choices declared outside the if statement so they can be concatenated upon condition*
+
+var choices;
+
+// converts letters to uppercase*
+
+var toUpper = function (x) {
+
+return x.toUpperCase();
+
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
