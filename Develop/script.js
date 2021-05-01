02909bcbbe1c33004de9b2709d4f2f39e5fc89f3
Melissa Santos <melissasantos@Melissas-MBP.lan>
2021-05-01 14:30:48 -0400

    updated generate password function in script.js

--- Develop/script.js
+++ Develop/script.js
@@ -10,29 +10,28 @@ var confirmUppercase;
 
 var confirmLowercase;
 
-// Start Password variable values:*
-
-// Special characters*
+// Password variable values:
+// Special characters
 
 character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
-// Numeric characters*
+// Numeric characters
 
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// Alphabetical characters*
+// Alphabetical characters
 
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Space is for the Uppercase conversion*
+// Space is for the Uppercase conversion
 
 space = [];
 
-// Choices declared outside the if statement so they can be concatenated upon condition*
+// choices declared outside if statement so they can be concatenated upon condition
 
 var choices;
 
-// converts letters to uppercase*
+// converts letters to uppercase
 
 var toUpper = function (x) {
 
@@ -40,17 +39,35 @@ return x.toUpperCase();
 
 };
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// creates variable for uppercase conversion
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+alpha2 = alpha.map(toUpper);
 
-  passwordText.value = password;
+var get = document.querySelector("#generate");
 
+get.addEventListener("click", function () {
+
+ps = generatePassword();
+
+document.getElementById("password").placeholder = ps;
+
+});
+
+//function to generate password
+function generatePassword () {
+  //user input
+  enter = parseInt(prompt("What length would you like the password to be? (Between 8 to 128 characters)"));
+}
+// if statement for user validation
+if (!enter) {
+  alert("This needs a value");
+} 
+
+else if (enter < 8 || enter > 128) {
+  enter = parseInt(prompt("You must choose between 8 and 128"));
+} 
+//user input validated
+else {
+confirmNumber= confirm("Would you like it to contain numbers?")
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
