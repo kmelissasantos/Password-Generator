0f022a9d33d8b4d22332d69aae3e4e8019c5ae7b
Melissa Santos <melissasantos@melissas-mbp.attlocal.net>
2021-05-10 22:37:54 -0400

    updated README.md

--- Develop/script.js
+++ Develop/script.js
@@ -8,13 +8,13 @@ var confirmLower;
 var userChoices;
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Var To Upper Case ------------
+// Var To Upper Case
 var blankUpper = [];
 var toUpper = function (x) {
   return x.toUpperCase();
 };
 upperCase = lowerCase.map(toUpper);
-//--------------------------------------
+
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Password Generator Starter Code
+# Password Generator
+HTML and CSS and Javascript documents create a random password generator. This project emphasizes the use of using Javascript to make dynamic changes to an HMTL document
+
+## Url to the website: 
+
+
+## Usage
+Coded with HTML, CSS, & Javascript
+
